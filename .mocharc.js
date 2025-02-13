a555465280ace5deffd33848c37e5adc28dcb158
GenkoPuhov <genkopuhov@abv.bg>
2025-02-13 12:02:37 +0200

    fix

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,4 @@
-// .mocharc.js - Mocha config file samo chance
+// .mocharc.js - Mocha config file samo chances
 const {colors, symbols} = 
   require('mocha/lib/reporters/base');
 // Change the default console UI colors
