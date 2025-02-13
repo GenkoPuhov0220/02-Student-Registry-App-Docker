4704fc71f2b9ebe03e9753014fd96e90bfec45ff
GenkoPuhov <genkopuhov@abv.bg>
2025-02-13 12:35:26 +0200

    promqna

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,4 @@
-// .mocharc.js - Mocha config file samo chances dddd
+// .mocharc.js - Mocha config file samo chances dddd promqna
 const {colors, symbols} = 
   require('mocha/lib/reporters/base');
 // Change the default console UI colors
